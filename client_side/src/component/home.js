67d16678a7fbd7bd520e9ca6f47f22ca2de363bd
kuldeep <kuldeep.singh.cs23@iilmcet.ac.in>
2022-10-06 02:28:08 +0530

    update3

--- client_side/src/component/home.js
+++ client_side/src/component/home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Task from '../images/task.jpg'
 const home = () => {
   return (
-    <div className='  justify-center items-center text-center flex  h-screen p-[20px] bg-pink-600 mt-16 home '>
+    <div className='  justify-center items-center text-center flex  h-screen p-[20px] bg-indigo-600 mt-16 home '>
       <div className=' first-letter:text-8xl text-5xl  text-white mx-3'>Plan your task according to your requirements and priority</div>
       <img  className=' rounded-xl mx-4' src={Task}/>
     </div>
