834e71748b2e9dce2d705e0bf3e8a68c2b4d17e2
kuldeep <kuldeep.singh.cs23@iilmcet.ac.in>
2022-10-06 05:30:33 +0530

    update2

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ require('./database_mongoose/DBconnection');
 //post request
 app.use(require('./router/auth'));
 
-if (process.env.NODE_ENV == "production") {
+if (process.env.NODE_ENV === "production") {
     app.use(express.static("client_side/build"));
     app.get("/*", function (req, res) {
         res.sendFile(path.join(__dirname, "./client_side/build/index.html"))
