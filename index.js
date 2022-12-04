85ef23f9b0910ab239b634d8460056391c23fd31
kuldeep <kuldeep.singh.cs23@iilmcet.ac.in>
2022-12-04 21:04:34 +0530

    update

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(require('./router/auth'));
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client_side/build"));
-    app.get("/*", function (req, res) {
+    app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "./client_side/build/index.html"))
 })
 }
